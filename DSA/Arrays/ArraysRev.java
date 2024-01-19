310f779e93bf7879ccd6c5fe614904fd253ab1ff
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-19 17:00:03 +0530

    arrays Rev

--- DSA/Arrays/ArraysRev.java
+++ DSA/Arrays/ArraysRev.java
@@ -3,10 +3,18 @@ package DSA.Arrays;
 public class ArraysRev {
     public static void main(String[] args) {
         int arr[]={1,3,2,5,4,7,6,9,8,0};
-        for (int i = 0; i < arr.length; i++) {
-            int temp=arr[i];
-            System.out.print(temp);
-//
+        int arraysKeyLength=arr.length;
+
+
+        for(int i=arraysKeyLength-1;  i>=0 ; i--){
+            System.out.print(arr[i]+" ");
+        }
+
+
+        int halfOfArray = (arr.length)/2;
+        System.out.print("\n the half Arrays \n"+halfOfArray);
+        for (int i = halfOfArray; i >=0 ; i--) {
+            System.out.print(arr[i]);
         }
     }
 }
