26123c57759819c7becd5b2d3c6a20d6e9c4be74
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-21 01:15:05 +0530

    Reverse each word

---
+++ DSA/CodingQuestion/ReverseEachWord.java
@@ -0,0 +1,23 @@
+package DSA.CodingQuestion;
+
+import java.util.*;
+public class ReverseEachWord {
+    public static void main(String[] args) {
+        System.out.println("Enter the String for  Reverse each word");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String arr[] = str.split(" ");
+        String ans ="";
+
+        for(int i=0;i<arr.length;i++){
+            String temp = arr[i];
+            String temp2 = "";
+
+            for(int j = temp.length()-1; j>=0; j--){
+                temp2 += temp.charAt(j);
+            }
+            ans+=temp2+" ";
+        }
+        System.out.print(ans);
+    }
+}
