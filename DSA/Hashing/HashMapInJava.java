93aa108e653533bcc84ac10ba60b1109ab03150d
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-21 21:17:34 +0530

    //Creating HashMap in Java

--- DSA/Hashing/HashMapInJava.java
+++ DSA/Hashing/HashMapInJava.java
@@ -1,7 +1,14 @@
 package DSA.Hashing;
 
+import java.util.HashMap;
+
 public class HashMapInJava {
     public static void main(String[] args) {
+        System.out.println("HashMap in Java");
+
+        //Creating HashMap in Java
+        HashMap<String,Integer> mapUserDefineName = new HashMap<>();
+
 
     }
 }
