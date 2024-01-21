8341caf86f3898615d15b5195d628196d711793e
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-22 04:05:15 +0530

    Remove duplicates

---
+++ DSA/CodingQuestion/RemoveDuplicates.java
@@ -0,0 +1,21 @@
+package DSA.CodingQuestion;
+
+
+import java.util.*;
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        HashSet<Character> hs = new HashSet<>();
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the Strings");
+        String str = sc.next();
+        for(int i =0;i<str.length();i++){
+            char ch = str.charAt(i);
+            if(hs.contains(ch)){
+                continue;
+            }else{
+                System.out.print(ch);
+                hs.add(ch);
+            }
+        }
+    }
+}
