6e58f0c46775f5578583848b05c454f024a63ec4
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-22 15:01:09 +0530

    Print the pattern 1

---
+++ DSA/CodingQuestion/PrintThePattern1.java
@@ -0,0 +1,20 @@
+package DSA.CodingQuestion;
+import java.util.*;
+public class PrintThePattern1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int N = sc.nextInt();
+        for(int i=1 ; i<=N; i++){
+            int temp = i;
+            for(int j = 1; j<=i;j++){
+                System.out.print(temp++ +" ");
+            }
+            temp-=2;
+            for(int j =1;j<i;j++){
+                System.out.print(temp-- +" ");
+            }
+            System.out.println();
+        }
+    }
+}
