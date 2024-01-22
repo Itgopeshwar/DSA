df0fbf5b7aa5310fc2ee67b6bee8a3b4e78bd723
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-23 04:45:31 +0530

    Factorial

---
+++ DSA/CodingQuestion/Factorial.java
@@ -0,0 +1,17 @@
+package DSA.CodingQuestion;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number which is find Factorial : ");
+        int input = sc.nextInt();
+        int ans = 1;
+        while(input > 1){
+            ans*=input;
+            input--;
+        }
+        System.out.print("Factorial is : "+ans);
+    }
+}
