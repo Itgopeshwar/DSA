8fd4663dd3db3fe8d0cf94d8d98053bc16be29e3
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-24 00:48:47 +0530

    Fibonacci Series using Recursion

---
+++ DSA/CodingQuestion/FibonacciSeriesUsingRecursion.java
@@ -0,0 +1,26 @@
+package DSA.CodingQuestion;
+
+import java.util.Scanner;
+
+public class FibonacciSeriesUsingRecursion {
+    public static void recur(int a,int b,int N){
+        if(N==0){
+            return;
+        }
+        System.out.print(a+b +" ");
+        recur(b,a+b,N-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number :  ");
+        int InputNumber= sc.nextInt();
+
+        if(InputNumber==1){
+            System.out.print(1);
+        }
+        else if(InputNumber > 2){
+            System.out.print(1+" "+1+" ");
+            recur(1,1,InputNumber-2);
+        }
+    }
+}
