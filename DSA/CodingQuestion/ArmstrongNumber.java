0c3b4c88906c50e50cc98c8acbbc0ba92970090d
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-24 00:13:42 +0530

    Armstrong Number

---
+++ DSA/CodingQuestion/ArmstrongNumber.java
@@ -0,0 +1,30 @@
+package DSA.CodingQuestion;
+import java.util.Scanner;
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number");
+        int InputNumber= sc.nextInt();
+        int temp = InputNumber;
+        int count = 0;
+
+        while(temp!=0){
+            temp=temp/10;
+            count++;
+        }
+        temp=InputNumber;
+        int sum =0;
+        while(temp!=0){
+            int digit = temp%10;
+            sum+= (int) Math.pow(digit,count);
+            temp=temp/10;
+
+        }
+        if(sum==InputNumber){
+            System.out.print("Armstrong");
+        }
+        else{
+            System.out.print("Not Armstrong");
+        }
+    }
+}
