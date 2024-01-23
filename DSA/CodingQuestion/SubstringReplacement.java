b6b014a9bf71b98fc67d1e597572a42307d5cb91
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-23 23:46:41 +0530

    Substring Replacement

---
+++ DSA/CodingQuestion/SubstringReplacement.java
@@ -0,0 +1,21 @@
+package DSA.CodingQuestion;
+
+import java.util.Scanner;
+
+public class SubstringReplacement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("original String Input");
+        String orignalString = sc.nextLine();
+
+        System.out.println("Old String Input");
+        String oldString = sc.nextLine();
+
+        System.out.println("new String Input");
+        String newString = sc.nextLine();
+
+        String ans = orignalString.replace(oldString,newString);
+        System.out.println(ans);
+    }
+}
