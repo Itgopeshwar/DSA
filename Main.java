639f2267fe5e317ffad88e3863fac3f6bf85750a
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-25 00:16:57 +0530

    support file or method creat and use

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
+import Src.support.support;
 public class Main {
     public static void main(String[] args) {
         System.out.println(" This is main ");
+
+        // import objects use
+        support obj=new support();
+
+        int sum = obj.Add(23,7);
+        System.out.println(sum);
+
+        int diff = obj.Sub(70,20);
+        System.out.println(diff);
+
     }
 }
---
+++ Src/support/support.java
@@ -0,0 +1,13 @@
+package Src.support;
+
+public class support {
+    public int Add(int first, int second){
+        int sum=first+second;
+        return sum;
+    }
+
+    public int Sub(int first, int second){
+        int sub=first-second;
+        return sub;
+    }
+}
