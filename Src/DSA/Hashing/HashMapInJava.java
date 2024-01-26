e14df30ec1783756343e38cb07bc0dd6ac53f2fc
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-27 04:43:07 +0530

    hash map iteration

--- Src/DSA/Hashing/HashMapInJava.java
+++ Src/DSA/Hashing/HashMapInJava.java
@@ -1,6 +1,7 @@
 package Src.DSA.Hashing;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class HashMapInJava {
     public static void main(String[] args) {
@@ -21,26 +22,34 @@ public class HashMapInJava {
 
 
         // Search
-//        if (mapUserDefineName.containsKey("China")){
-//            System.out.println("key is present in the map");
-//        }else {
-//            System.out.println("key is not present in the map");
-//        }
+        if (mapUserDefineName.containsKey("China")){
+          //  System.out.println("key is present in the map");
+        }else {
+            //System.out.println("key is not present in the map");
+        }
 
-//        System.out.println(mapUserDefineName.get("China"));
-//        System.out.println(mapUserDefineName.get("UK"));
+        // System.out.println(mapUserDefineName.get("China"));
+        //System.out.println(mapUserDefineName.get("UK"));
 
         // for loop type
         int[] arr ={12,13,45};
         // first type
         for (int i = 0; i <arr.length ; i++) {
-            System.out.print(arr[i]+" ");
+//            System.out.print(arr[i]+" ");
         }
         System.out.print("\n"); //line change
 
         //second type
         for(int value_of_Arr:arr){
-            System.out.print(value_of_Arr+" ");
+//            System.out.print(value_of_Arr+" ");
+        }
+
+        // hashMap iteration
+        for (Map.Entry<String,Integer> e :mapUserDefineName.entrySet()){
+            // e => key as well as value both are hold in e variable
+            System.out.print(e.getKey()+" ");
+            System.out.print(e.getValue()+" ");
+            System.out.println();
         }
 
     }
