382a2b345a78eab7c173c05dd2e7e8ca6669f331
Mohsin <mdmohsinhaider6@gmail.com>
2024-02-17 23:47:53 +0530

    rehshing fun

--- Src/DSA/Hashing/HashMapCode.java
+++ Src/DSA/Hashing/HashMapCode.java
@@ -1,5 +1,7 @@
 package Src.DSA.Hashing;
 
+import java.util.LinkedList;
+
 public class HashMapCode {
     static class HasMap<K,V>{
         K key;
@@ -25,8 +27,25 @@ public class HashMapCode {
     }
 
 
-    private int hashFunction(k key){
+    private int hashFunction(K key){ // 0-N-1
+        int bi=key.hashCode();
+        return Math.abs(bi) %N;
+    }
 
+    private int searchInLL(K key ,int bi){
+        LinkedList<Node> ll = buckets[bi];
+        int di = 0;
+        for(int i=0; i<ll.size();i++){
+            if(ll.get(i).key == key){
+                return i; // di
+            }
+        }
+        return -1;
+    }
+
+    private void rehash(){
+        LinkedList<Node> oldBucket[]=buckets;
+        buckets=new LinkedList[N*2];
     }
     public void put(K key,V value){
         int bi = hashFunction(key);
@@ -36,13 +55,13 @@ public class HashMapCode {
             buckets[bi].add(new Node(key,value));
             n++;
         }else { // key exits
-            Node data = buckets[bi].get(di);
-            data.value=value;
+            Node node = buckets[bi].get(di);
+            node.value=value;
         }
 
         double lambda = (double)n/N;
         if(lambda>2.0){
-            // rehashing
+            rehash();
 
         }
     }
