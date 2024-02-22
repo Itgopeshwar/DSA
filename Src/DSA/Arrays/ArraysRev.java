d250a2f935cf78fac6eed6d580f67bae65b0ede7
Mohsin <mdmohsinhaider6@gmail.com>
2024-02-23 01:30:47 +0530

    Liner array

--- Src/DSA/Arrays/ArraysRev.java
+++ Src/DSA/Arrays/ArraysRev.java
@@ -2,14 +2,13 @@ package Src.DSA.Arrays;
 
 public class ArraysRev {
     public static void main(String[] args) {
-        int arr[]={1,3,2,5,4,7,6,9,8,0};
-        int arraysKeyLength=arr.length;
 
+        int[] arr ={1,3,2,5,4,7,6,9,8,0};
+        int arraysKeyLength=arr.length;
 
         for(int i=arraysKeyLength-1;  i>=0 ; i--){
             System.out.print(arr[i]+" ");
         }
-
 
         int halfOfArray = (arr.length)/2;
         System.out.print("\n the half Arrays \n"+halfOfArray);
---
+++ Src/DSA/SearchAlgo/SwapBetweenSize.java
@@ -0,0 +1,18 @@
+package Src.DSA.SearchAlgo;
+
+public class SwapBetweenSize {
+
+    void SwapNum(int[] arr,int size){
+        //ittrating 
+        for (int i = 0; i < arr.length; i+=2) {
+            if (i+1 < size) {
+                swap(arr[i],arr[i+1]);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {2,8,6,4,3,8,9,5,7};
+        int[] arrOdd = {23,45,67,89,20,42,62};
+
+    }
+}
