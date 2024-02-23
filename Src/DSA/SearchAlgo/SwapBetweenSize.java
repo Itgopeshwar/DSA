8a22a35a49c20c80c49de56121fb4ced3446d818
Mohsin <mdmohsinhaider6@gmail.com>
2024-02-23 23:57:54 +0530

    Swaping number in arrray

--- Src/DSA/SearchAlgo/SwapBetweenSize.java
+++ Src/DSA/SearchAlgo/SwapBetweenSize.java
@@ -6,7 +6,7 @@ public class SwapBetweenSize {
         //ittrating 
         for (int i = 0; i < arr.length; i+=2) {
             if (i+1 < size) {
-                swap(arr
+
             }
         }
     }
