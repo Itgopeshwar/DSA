5e5fc88b4f445efa57c3889037e5abb88bafb809
Mohsin <mdmohsinhaider6@gmail.com>
2024-02-26 22:46:48 +0530

    Data Type

--- Src/DSA/HakerRank/DataType.java
+++ Src/DSA/HakerRank/DataType.java
@@ -1,8 +1,25 @@
 package Src.DSA.HakerRank;
 
+import java.util.Scanner;
+
 public class DataType {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt(); // input frome zero t
+
+        for(int i=0;i<t;i++){
+            try{
+                long x=sc.nextLong();
+                System.out.println(x+" can be fitted in:");
+                if(x>=-128 && x<=127)System.out.println("* byte");
+                //Complete the code
+            }
+            catch(Exception e){
+                System.out.println(sc.next()+" can't be fitted anywhere.");
+            }
+            sc.close();
+
+        }
     }
 }
 
