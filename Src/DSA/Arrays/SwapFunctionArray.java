827d145b922f167200c4b4982f8eb4e07b520eda
Mohsin <mdmohsinhaider6@gmail.com>
2024-02-27 15:47:51 +0530

    swap

---
+++ Src/DSA/Arrays/SwapFunctionArray.java
@@ -0,0 +1,16 @@
+package Src.DSA.Arrays;
+
+public class SwapFunctionArray {
+
+    void swapFun(int arr[],int size){
+        for (int i = 0; i < arr.length; i++) {
+            
+        }
+    }
+    public static void main(String[] args) {
+        int array[]={5,3,2,5,7,4,5,3,2,5,6,7,8,6,4,3};
+        SwapFunctionArray swap_obj=new SwapFunctionArray();
+        swap_obj(array[],6);
+    }
+    
+}
