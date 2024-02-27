127dad5cf0b4fc51176d0f086880a7a974bd3c2b
Mohsin <mdmohsinhaider6@gmail.com>
2024-02-27 19:53:20 +0530

    Bittwice example

--- Src/DSA/Arrays/ArraysXORcondition.java
+++ Src/DSA/Arrays/ArraysXORcondition.java
@@ -1,2 +1,16 @@
-package Src.DSA.Arrays;public class ArraysXORcondition {
+package Src.DSA.Arrays;
+
+public class ArraysXORcondition {
+    public static void main(String[] args) {
+        // int arrr[] = {1,2,3,5,4,5,6,7,8,9,10};
+        // int ans=0;
+        // for (int i = 0; i < arrr.length; i++) {
+        //     ans=ans^arrr[i];
+        //     System.out.println(i+"    "+ arrr[i] +"    "+ ans + "    ");
+        // }
+
+        int a = 5;
+        int b = 7;
+        System.out.println(a^b);
+    }
 }
